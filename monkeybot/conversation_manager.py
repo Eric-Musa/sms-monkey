269cfd7a46a4c63c6bc4eefb40bcc59db80c03a8
Debian <bitnami@ip-172-26-10-90.ec2.internal>
2024-01-07 02:46:35 +0000

    fixed parallel conversation support, python hashes change from session to session so I changed the hash method for obfuscating phone numbers. Next step is queueing for when traffic to the model is very high

--- monkeybot/conversation_manager.py
+++ monkeybot/conversation_manager.py
@@ -1,11 +1,13 @@
 import dotenv
 dotenv.load_dotenv()
+import hashlib
 import json
 import openai
 import os
 import re
 import requests
 import time
+
 
 openai.api_key = openai_api_key = os.environ['OAI_SERVER_API_KEY']
 if 'HOMEBREW_PREFIX' in os.environ:
@@ -37,23 +39,36 @@ class ConversationManager:
     FILENAME_TEMPLATE_RE = 'monkeybot-(\\d+)-(\\d+).txt'
     
     
-    # def __init__(self, convo_directory='conversations') -> None:
-    # def __init__(self, ) -> None:
-        # if not os.path.isdir(convo_directory):
-            # os.mkdir(convo_directory)
-        # self.convo_directory = convo_directory
-        # self.messages, self.filename = self.load_conversation()
-
+    def __init__(self, convo_directory='conversations', verbose=True) -> None:
+        if not os.path.isdir(convo_directory):
+            os.mkdir(convo_directory)
+        self.convo_directory = convo_directory
+        self.verbose = verbose
+    
     @staticmethod
     def hash_number(user_number):
-        return str(hash(str(user_number)))
+            # Choose a hash function. Here we use SHA256.
+        hash_object = hashlib.sha256()
+        
+        # Update the hash object with the user ID.
+        # The user ID should be encoded to bytes.
+        hash_object.update(user_number.encode())
+        
+        # Return the hexadecimal digest of the user ID.
+        return hash_object.hexdigest()
+        # return str(hash(str(user_number)))
 
-    def get_latest_conversation(self, number_hash):
+    @staticmethod
+    def get_model(headers=auth_headers):
+        return requests.get(url=f'{openai_api_base}/model', headers=headers).json()['model_name']
+
+    def get_latest_conversation(self, conversation_directory):
         latest_filenum = -1
         ts = None
-        if not os.path.isdir(number_hash):
-            os.mkdir(number_hash)
-        for fname in os.listdir(number_hash):
+        if not os.path.isdir(conversation_directory):
+            print(f'Created conversation directory: {conversation_directory}', flush=True)
+            os.mkdir(conversation_directory)
+        for fname in os.listdir(conversation_directory):
             result = re.findall('monkeybot-(\\d+)-(\\d+).txt', fname)
             if result:
                 assert len(result) == 1, f'should only be one match in {fname}'
@@ -63,68 +78,66 @@ class ConversationManager:
                     ts = timestamp
         return latest_filenum, ts or int(time.time())
 
-    def load_conversation(self, number_hash):
-        latest_filenum, timestamp = self.get_latest_conversation(number_hash)
+    def load_conversation(self, conversation_directory):
+        latest_filenum, timestamp = self.get_latest_conversation(conversation_directory)
         if latest_filenum == -1:
             filename = self.FILENAME_TEMPLATE.format(1, timestamp)
             print(f'No existing conversation files found, creating {filename}')
-            with open(os.path.join(number_hash, filename), 'w') as f:
+            with open(os.path.join(conversation_directory, filename), 'w') as f:
                 messages = list(self.PRIMER)
                 json.dump(messages, f)
                 for message in messages: self.print_message(message)
         else:
             filename = self.FILENAME_TEMPLATE.format(latest_filenum, timestamp)
             print(f'Prior conversation found, {filename}')
-            with open(os.path.join(number_hash, filename), 'r') as f:
+            with open(os.path.join(conversation_directory, filename), 'r') as f:
                 messages = json.load(f)
                 for message in messages: self.print_message(message)
         return messages, filename
 
-    def reset_conversation(self, number_hash):
-        latest_filenum, timestamp = self.get_latest_conversation(number_hash)
+    def reset_conversation(self, conversation_directory):
+        latest_filenum, timestamp = self.get_latest_conversation(conversation_directory)
         if latest_filenum == -1:
             filename = self.FILENAME_TEMPLATE.format(1, timestamp)
             print(f'No existing conversation files found, creating {filename}')
         else:
             filename = self.FILENAME_TEMPLATE.format(latest_filenum+1, int(time.time()))
             print(f'Creating new conversation, {filename}')
-        with open(os.path.join(number_hash, filename), 'w') as f:
+        with open(os.path.join(conversation_directory, filename), 'w') as f:
             json.dump(self.PRIMER, f)
         for message in self.PRIMER: self.print_message(message)
         # self.messages = list(self.PRIMER)
         # self.filename = filename
 
-    def get_model(self, headers=auth_headers):
-        return requests.get(url=f'{openai_api_base}/model', headers=headers).json()['model_name']
 
     def complete_chat(self, user_input, user_number, max_tokens=64):
         number_hash = self.hash_number(user_number)
+        conversation_directory = os.path.join(self.convo_directory, number_hash)
         if user_input == 'RESTART':
             time.sleep(0.3)
             print()
             print('RESETTING MESSAGE HISTORY')
             print()
-            self.reset_conversation(number_hash)
-            return 'CONVERSATION RESET'
+            self.reset_conversation(conversation_directory)
+            return f'Conversation Reset... {self.PRIMER[-1]["content"]}', len(self.PRIMER)
         
-        messages, filename = self.get_latest_conversation(number_hash)
+        messages, filename = self.load_conversation(conversation_directory)
         messages.append({'role': 'user', 'content': user_input})
         
-        chat_completion = openai.ChatCompletion.create(model=self.get_model(), messages=self.messages, max_tokens=max_tokens)
+        chat_completion = openai.ChatCompletion.create(model=self.get_model(), messages=messages, max_tokens=max_tokens)
         ai_response = chat_completion.choices[0].message.content
         trimmed_ai_response = self.trim_incomplete_response(ai_response, comma_is_delimiter=True)
         
         messages.append({'role': 'assistant', 'content': trimmed_ai_response})
         # messages.append({'role': 'AI', 'content': trimmed_ai_response})
         self.print_message(messages[-1])
-        with open(os.path.join(number_hash, filename), 'w') as f:
+        with open(os.path.join(conversation_directory, filename), 'w') as f:
             json.dump(messages, f)
         return trimmed_ai_response, len(messages)
 
-    @staticmethod
-    def print_message(message):
+    def print_message(self, message):
         assert isinstance(message, dict) and all(_ in message for _ in ['role', 'content'])
-        print(f"{message['role']}: {message['content']}")
+        if self.verbose: print(f"{message['role']}: {message['content']}")
 
     @staticmethod
     def trim_incomplete_response(response, delimiters=('.', '!', '?'), comma_is_delimiter=False):
